818125b086ab4d7b9e4edb9d008b780cb49b689b
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-11-27 21:21:07 +0300

    lesson 1.1 end

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
 import React from 'react';
 import './App.css';
+import {TaskType, TodoList} from "./TodoList";
 
-function App() {
+
+export function App() {
+    const tasks_1: TaskType[] = [
+        {id: 1, isDone: true, taskTitle: 'HTML&CSS'},
+        {id: 2, isDone: true, taskTitle: 'JS/TS'},
+        {id: 3, isDone: false, taskTitle: 'REACT'},
+        {id: 4, isDone: true, taskTitle: 'REDUX'},
+    ]
+    const tasks_2: TaskType[] = [
+        {id: 5, isDone: true, taskTitle: 'Bread'},
+        {id: 6, isDone: false, taskTitle: 'Chocolate'},
+        {id: 7, isDone: true, taskTitle: 'Tea'},
+        {id: 8, isDone: false, taskTitle: 'Coffee'},
+    ]
+
     return (
         <div className="App">
-            <div className={'todolist'}>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <TodoList todoListTitle={'Что купить'} tasks={tasks_1}/>
+            <TodoList todoListTitle={'Что прочитать'} tasks={tasks_2}/>
         </div>
     );
 }
 
-export default App;
