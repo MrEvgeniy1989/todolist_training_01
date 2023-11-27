dd013afa728afe8301a466483afd958db81203a0
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-11-27 21:47:04 +0300

    dev lesson 1.1 end

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
 import React from 'react';
 import './App.css';
+import {TaskType, TodoLIst} from "./TodoLIst";
 
-function App() {
+export const App = () => {
+    const tasks_1: TaskType[] = [
+        {id: 1, isDone: true, taskTitle: 'HTML'},
+        {id: 2, isDone: true, taskTitle: 'CSS'},
+        {id: 3, isDone: false, taskTitle: 'JS'},
+        {id: 4, isDone: true, taskTitle: 'REACT'},
+    ]
+    const tasks_2: TaskType[] = [
+        {id: 5, isDone: false, taskTitle: 'Bread'},
+        {id: 6, isDone: true, taskTitle: 'Milk'},
+        {id: 7, isDone: false, taskTitle: 'Chocolate'},
+        {id: 8, isDone: true, taskTitle: 'Butter'},
+    ]
+
     return (
         <div className="App">
-            <div className={'todolist'}>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <TodoLIst todoListTitle={'Что изучить'} tasks={tasks_1}/>
+            <TodoLIst todoListTitle={'Что купить'} tasks={tasks_2}/>
         </div>
-    );
+    )
 }
-
-export default App;
---
+++ src/TodoLIst.tsx
@@ -0,0 +1,34 @@
+import React, {FC} from 'react';
+
+export type TaskType = {
+    id: number
+    isDone: boolean
+    taskTitle: string
+}
+
+type TodoLIstProps = {
+    todoListTitle: string
+    tasks: TaskType[]
+}
+
+export const TodoLIst: FC<TodoLIstProps> = ({todoListTitle, tasks}) => {
+    return (
+        <div className={'todolist'}>
+            <h3>{todoListTitle}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                <li><input type="checkbox" checked={tasks[0].isDone}/> <span>{tasks[0].taskTitle}</span></li>
+                <li><input type="checkbox" checked={tasks[1].isDone}/> <span>{tasks[1].taskTitle}</span></li>
+                <li><input type="checkbox" checked={tasks[2].isDone}/> <span>{tasks[2].taskTitle}</span></li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    );
+}
